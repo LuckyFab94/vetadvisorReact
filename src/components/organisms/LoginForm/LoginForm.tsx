970d9d38648f55ad11685d99b13cd5165eb29f02
Fabrizio <cerrattifabrizio@gmail.com>
2022-01-17 20:48:42 +0100

    modifiche signup & login

--- src/components/organisms/LoginForm/LoginForm.tsx
+++ src/components/organisms/LoginForm/LoginForm.tsx
@@ -1,22 +1,25 @@
+import { FormControl, InputBase, InputLabel } from '@mui/material'
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { useAuth } from '../../../context/AuthContext'
+import { BootstrapInput } from '../../atoms/BootstrapInput/BootstrapInput'
 import Button from '../../atoms/Button/Button'
-import TextInput from '../../atoms/TextInput/TextInput'
 import styleLoginForm from './LoginForm.module.scss'
 
 const LoginForm = () => {
-    const [formData, setFormData] = useState({nome:'',email:'',password:''})
+    const [formData, setFormData] = useState({ nome: '', email: '', password: '' })
     const auth = useAuth()
     const [error, setError] = useState('')
-    const Login = async(e: React.SyntheticEvent) => {
+    const Login = async (e: React.SyntheticEvent) => {
         e.preventDefault()
-        try{
-            await auth?.login(formData.email,formData.password)
-       }catch(e: any){
-           setError(e.code)
-       }
+        try {
+            await auth?.login(formData.email, formData.password)
+        } catch (e: any) {
+            setError(e.code)
+        }
     }
+
+    console.log(formData)
 
     return (
         <form className={styleLoginForm.form} onSubmit={Login}>
@@ -25,16 +28,28 @@ const LoginForm = () => {
                 {auth?.currentUser?.email}
                 {error !== '' && <p>{error}</p>}
                 <div className={styleLoginForm.formGroup}>
-                    <label htmlFor='nome' >Nome:</label>
-                    <TextInput type={'text'} name='nome' id='nome' value={formData.nome} onChange={(e: any)=>setFormData({...formData, nome: e.target.value})}/>
+                    <FormControl style={{ display: 'flex' }} variant="standard">
+                        <InputLabel shrink htmlFor="nome">
+                            Nome:
+                        </InputLabel>
+                        <BootstrapInput id="nome" onChange={(e: any) => setFormData({ ...formData, nome: e.target.value })} />
+                    </FormControl>
                 </div>
                 <div className={styleLoginForm.formGroup}>
-                    <label htmlFor='email' >Email:</label>
-                    <TextInput type={'email'} name='email' id='email' value={formData.email} onChange={(e: any)=>setFormData({...formData, email: e.target.value})}/>
+                    <FormControl style={{ display: 'flex' }} variant="standard">
+                        <InputLabel shrink htmlFor="email">
+                            Email:
+                        </InputLabel>
+                        <BootstrapInput id="email" onChange={(e: any) => setFormData({ ...formData, email: e.target.value })} />
+                    </FormControl>
                 </div>
                 <div className={styleLoginForm.formGroup}>
-                    <label htmlFor='password' >Password:</label>
-                    <TextInput type={'password'} name='password' id='password' value={formData.password} onChange={(e: any)=>setFormData({...formData, password: e.target.value})}/>
+                    <FormControl style={{ display: 'flex' }} variant="standard">
+                        <InputLabel shrink htmlFor="password">
+                            Password:
+                        </InputLabel>
+                        <BootstrapInput id="password" onChange={(e: any) => setFormData({ ...formData, password: e.target.value })} />
+                    </FormControl>
                 </div>
                 <Button className={styleLoginForm.buttonSubmit} type={'submit'} text={'LOGIN'} />
                 <p>Non hai un account? <Link to={'/vetadvisorReact/signup'}>Registrati</Link></p>
--- src/components/organisms/SignUpForm/SignUpForm.tsx
+++ src/components/organisms/SignUpForm/SignUpForm.tsx
@@ -1,23 +1,25 @@
+import { FormControl, InputLabel } from '@mui/material'
 import React, { useState } from 'react'
 import { useAuth } from '../../../context/AuthContext'
+import { BootstrapInput } from '../../atoms/BootstrapInput/BootstrapInput'
 import Button from '../../atoms/Button/Button'
 import TextInput from '../../atoms/TextInput/TextInput'
 import styleSignUpForm from './SignUpForm.module.scss'
 
 const SignUpForm = () => {
-    const [formData, setFormData] = useState({email:'',password:'', confirmPassword: ''})
+    const [formData, setFormData] = useState({ email: '', password: '', confirmPassword: '' })
     const auth = useAuth()
     const [error, setError] = useState('')
     const [loading, setLoading] = useState(false)
 
     const SignUp = async (e: React.SyntheticEvent) => {
         e.preventDefault()
-        if(formData.password !== formData.confirmPassword){
+        if (formData.password !== formData.confirmPassword) {
             return setError('le password non sono uguali')
         }
-        try{
-             await auth?.signup(formData.email,formData.password)
-        }catch(e: any){
+        try {
+            await auth?.signup(formData.email, formData.password)
+        } catch (e: any) {
             setError(e.code)
         }
     }
@@ -29,16 +31,28 @@ const SignUpForm = () => {
                 {auth?.currentUser?.email}
                 {error !== '' && <p>{error}</p>}
                 <div className={styleSignUpForm.formGroup}>
-                    <label htmlFor='email' >Email:</label>
-                    <TextInput type={'email'} name='email' id='email' value={formData.email} onChange={(e: any)=>setFormData({...formData, email: e.target.value})}/>
+                    <FormControl style={{ display: 'flex' }} variant="standard">
+                        <InputLabel shrink htmlFor="email">
+                            Email:
+                        </InputLabel>
+                        <BootstrapInput id="email" onChange={(e: any) => setFormData({ ...formData, email: e.target.value })} />
+                    </FormControl>
                 </div>
                 <div className={styleSignUpForm.formGroup}>
-                    <label htmlFor='password' >Password:</label>
-                    <TextInput type={'password'} name='password' id='password' value={formData.password} onChange={(e: any)=>setFormData({...formData, password: e.target.value})}/>
+                    <FormControl style={{ display: 'flex' }} variant="standard">
+                        <InputLabel shrink htmlFor="password">
+                            Password:
+                        </InputLabel>
+                        <BootstrapInput id="password" onChange={(e: any) => setFormData({ ...formData, password: e.target.value })} />
+                    </FormControl>
                 </div>
                 <div className={styleSignUpForm.formGroup}>
-                    <label htmlFor='confirmpassword' >Confirm Password:</label>
-                    <TextInput type={'password'} name='confirmpassword' id='confirmpassword' value={formData.confirmPassword} onChange={(e: any)=>setFormData({...formData, confirmPassword: e.target.value})}/>
+                    <FormControl style={{ display: 'flex' }} variant="standard">
+                        <InputLabel shrink htmlFor="confirmpassword">
+                            Confirma Password:
+                        </InputLabel>
+                        <BootstrapInput id="confirmpassword" onChange={(e: any) => setFormData({ ...formData, confirmPassword: e.target.value })} />
+                    </FormControl>
                 </div>
                 <Button disabled={loading} className={styleSignUpForm.buttonSubmit} type={'submit'} text={'SIGNUP'} />
             </div>
