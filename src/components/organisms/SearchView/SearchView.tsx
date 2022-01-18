80500abb9861c262139f03d2cd57a80fd2fbc4fc
Fabrizio <cerrattifabrizio@gmail.com>
2022-01-18 21:56:04 +0100

    close icon in searchView

--- src/components/organisms/SearchView/SearchView.tsx
+++ src/components/organisms/SearchView/SearchView.tsx
@@ -1,9 +1,11 @@
+import { IconButton } from '@mui/material'
 import React, { useState } from 'react'
 import { RootStateOrAny, useSelector } from 'react-redux'
 import { closeSearch } from '../../../actions'
 import styleSearchView from './SearchView.module.scss'
+import CloseIcon from '@mui/icons-material/Close';
 
-const SearchView = ({search}: any) => {
+const SearchView = ({ search }: any) => {
     const [veterinari, setveterinari] = useState([])
     const showSearchView = useSelector((state: RootStateOrAny) => state.visibilityFilter)
     return (
@@ -12,7 +14,9 @@ const SearchView = ({search}: any) => {
                 showSearchView !== 'NOT_SHOWED' &&
                 <div className={styleSearchView.containerSearchView}>
                     <div className={styleSearchView.buttonEnd}>
-                        <button onClick={()=>closeSearch()}>x</button>
+                        <IconButton aria-label="delete" onClick={() => closeSearch()}>
+                            <CloseIcon />
+                        </IconButton>
                     </div>
                     <p>{search.nome} {search.localita}</p>
                     {
