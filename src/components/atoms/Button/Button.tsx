4bae0d89f4a2ef96a855a8f6b8e1b4f2c5546716
Fabrizio <fabrizio.cerratti.l@gmail.com>
2022-01-25 15:21:29 +0100

    change button style

--- src/components/atoms/Button/Button.tsx
+++ src/components/atoms/Button/Button.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import styleButton from './Button.module.scss'
+import Button from '@mui/material/Button';
 
-const Button = ({className, text, onClick, type, disabled}: any) => {
+const CustomButton = ({className, text, onClick, type, disabled}: any) => {
     return (
-        <button disabled={disabled} type={type} className={`${styleButton.button} ${className}`} onClick={onClick}>
+        <Button variant="contained" disabled={disabled} type={type} className={`${styleButton.button} ${className}`} onClick={onClick}>
             {text ?? 'Bottone'}
-        </button>
+        </Button>
     )
 }
 
-export default Button
+export default CustomButton
